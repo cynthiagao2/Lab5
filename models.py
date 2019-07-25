ac7fc0fc6f9bb6fbc8ea019121eb6ba5dcbc7212
Cynthia Gao <cynthiagao2@gmail.com>
2019-07-25 15:53:06 -0400

    Changes to forms.py, models.py, routes.py and implemented login and signup HTML pages

--- models.py
+++ models.py
@@ -1,3 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class User(db.Model):
+	__tablename__ = 'users'
+	uid = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	username = db.Column(db.String(64), unique=True, nullable=False)
+	password = db.Column(db.String(128), nullable=False)
--- routes.py
+++ routes.py
@@ -6,21 +6,64 @@ from passlib.hash import sha256_crypt
 app = Flask(__name__)
 app.secret_key = "s14a"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/lab_5'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/lab_5'
 
 db.init_app(app)
 
 @app.route('/')
 @app.route('/index')
 def index():
-	
+	if 'username' in session:
+		session_user = User.query.filter_by(username=session['username']).first()
+		return render_template('index.html', title='Home', session_username=session_user.username)
+	else:
+		return render_template('index.html', title='Home')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+	form = SignupForm()
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+
+		existing_user = User.query.filter_by(username=username).first()
+		
+		if existing_user:
+			flash('The username already exists. Please pick another one.')
+			return redirect(url_for('signup'))
+		
+		else:
+			user = User(username=username, password=sha256_crypt.hash(password))
+			db.session.add(user)
+			db.session.commit()
+			flash('Congratulations, you are now a registered user!')
+			return redirect(url_for('login'))
+	else:
+		return render_template('signup.html', title='Signup', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	
+	form = LoginForm()
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+
+		user = User.query.filter_by(username=username).first()
+
+			if user is None or not sha256_crypt.verify(password, user.password):
+				flash('Invalid username or password')
+				return redirect(url_for('login'))
+
+			else: 
+				session['username'] = username
+				return redirect(url_for('index'))
+	else: 
+		return render_template('login.html', title='Login', form=form)
     
 @app.route('/logout', methods=['POST'])
 def logout():
+	session.clear()
+	return redirect(url_for('index'))
 	
     
 @app.route('/newpost', methods=['GET', 'POST'])
